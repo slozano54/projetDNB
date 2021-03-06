d43f90c5a80d1dd4b878431931b596782c79cb11
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-03-06 18:11:42 +0100

    :sparkles: on force la compilation latex meme en cas d'erreur

--- tex_a_compiler/compilTex.sh
+++ tex_a_compiler/compilTex.sh
@@ -1,5 +1,7 @@
 # !/bin/bash
 
-latex $1.tex || exit
+#latex $1.tex || exit
+# On force la compilation même en cas d'erreur
+latex -halt-on-error $1 || exit
 dvips $1.dvi || exit
 ps2pdf $1.ps $1.pdf

94c5017562864498ad3300e28d6909afc1df2346
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-03-06 19:06:35 +0100

    :zap: compression à 0 pour les png

--- tex_a_compiler/dvi2png.sh
+++ tex_a_compiler/dvi2png.sh
@@ -1,4 +1,11 @@
 # !/bin/bash
 
+# pour ces commandes
+######### MAC OS ############
+#brew install netpbm
+#brew install poopler
+######### Linux #############
+# Je ne sais plus
+
 pdfcrop $1.pdf || exit
-pdftoppm $1-crop.pdf|pnmtopng > $1.png
+pdftoppm $1-crop.pdf|pnmtopng -compression 0 > $1.png

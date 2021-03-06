c8defdb2d359146a669464186051e40ce59a1fe4
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-03-06 09:12:58 +0100

    :sparkles: decoupage par exos version beta

--- pyPack/__init__.py
+++ pyPack/__init__.py
@@ -7,38 +7,52 @@
 # Génère la page d'accueil de la documentation pdoc3
 
 """
-    # PROJET DNB APMEP
-    Le projet consiste à récupérer tous les exercices des sujets DNB en partage sur le site de l'APMEP
+# PROJET DNB APMEP
 
-    Pour le moment le test se fait sur le premier exo du sujet de polynésie 2020
+Le projet consiste à récupérer tous les exercices des sujets DNB en partage sur le site de l'APMEP
 
-    Cette documentation a été générée avec le paquet **pdoc3**.
+Pour le moment le test se fait sur le premier exo du sujet de polynésie 2020
 
-    Utiliser le **package manager** [pip3](https://pip.pypa.io/en/stable/){:target="_blank"} pour l'installer.
+Cette documentation a été générée avec le paquet **pdoc3**.
 
-    ```bash
-    pip3 install pdoc3
-    ```
+Utiliser le **package manager** [pip3](https://pip.pypa.io/en/stable/) pour l'installer.
 
-    ## Installation et utilisation
+```bash
+pip3 install pdoc3
+```
+
+## Installation et utilisation
     
-    La procédure a été testé sous **Linux** uniquement.
+La procédure a été testé sous **Linux** uniquement.
 
-    * Cloner ce [dépot github](){:target="_blank"} ou télécharger [cette archive](){:target="_blank"}.
+* Cloner ce [dépot github](https://github.com/slozano54/projetDNB) ou télécharger [cette archive](https://github.com/slozano54/projetDNB/archive/master.zip).
+
+
+* À la racine du projet, lancer le script python **generate_all_files.py**.
+
+```bash
+python3 generate_all_files.py
+```
+
+* Sous **Visual Studio Code** lancer Live server et aller dans le dossier **PagesWeb**
     
-    * Décompresser l'archive.
+## Notes
 
-    * À la racine du projet, lancer le script python **generate_all_files.py**.
+Cette version n'est qu'un brouillon, elle n'est pas finalisée
 
-    ```bash
-    python3 generate_all_files.py
-    ```
+Pour le moment on peut uniquement déposer un fichier *.tex de corps d'exo dans le dossier **exercices_corrections_tex** puis lancer le script python **generate_all_files.py** pour obtenir :
 
-    * Sous **Visual Studio Code** lancer Live server et aller dans le dossier **PagesWeb**
-    
-    ## Notes
+* La documentation des fonctions du projet dans le dossier **docs/pyPack**
 
-    Cette version n'est qu'un brouillon, elle n'est pas finalisée
+* Un dossier **pagesWeb** avec une démo
+
+* un fichier *.tex prêt à compiler dans le dossier **tex_a_compiler**
+
+* un fichier pdf compilé dans le dossier **exercices_corrections_pdf**
+
+* un fichier pdf ajusté et compilé dans le dossier **exercices_corrections_pdf_crop**
+
+* un fichier png ajusté dans le dossier **exercices_corrections_png_jpg**
 
 """
 pass
--- pyPack/compil.py
+++ pyPack/compil.py
@@ -44,6 +44,9 @@ def generateTex(file : str,source_ex : str):
         mt.write(exo_lines[0:])
         mt.write(endDoc_lines[0:])
     
+    #On ferme les fichiers ouverts en écriture
+    myTex.close()
+    
 def compilTexToPDF(source : str):
     """
     Générer le fichier PDF à partir d'un fichier source tex
@@ -59,7 +62,6 @@ def compilTexToPDF(source : str):
     # On se remet à la racine du projet
     os.chdir("../")
 
-    
 def pdf2png(source : str):
     """
     Générer le fichier png ajusté et le pdf ajusté à partir d'un fichier source pdf
@@ -79,10 +81,6 @@ def pdf2png(source : str):
     # On se remet à la racine du projet
     os.chdir("../")
 
-
-
-
-
 def copyAllFiles(source : str):
     #On copie le fichier pdf
     #os.system("cp "+source+".pdf ../exercices_corrections_pdf/"+source+".pdf" )
@@ -94,24 +92,60 @@ def copyAllFiles(source : str):
     #On copie le fichier pdf ajusté
     os.system("cp ./tex_a_compiler/"+source+"-crop.pdf ./exercices_corrections_pdf_crop/"+source+"-crop.pdf" )
 
+def cutTex2Ex(file : str,source : str)->list:
+    """
+    Découpe un fichier source *.tex
+    
+    file : le début du nom du fichier dans lequel on va écrire
+    source : le fichier source contenant les exos
+    """
+
+    # On ouvre le fichier source    
+    with open("./sujets_corrections_tex/"+source+".tex","r") as source:
+        source_lines = source.readlines()
+
+    # Un compteur pour trouver les lignes contenant les entete des exos       
+    cpt = 0
+
+    # Un tableau pour les indices contenant le début des exos
+    indices = []
+
+    for i in range(len(source_lines)):
+        if ("textbf{Exercice" in source_lines[i]):
+            indices.append(i)
+    indices.append(len(source_lines)) 
+
+    for i in range(len(indices)-1):
+        # On ouvre le fichier dans lequel on va écrire
+        myTex = open("./exercices_corrections_tex/"+file+"_ex"+str(i+1)+".tex","w")
+        # On ajoute les lignes
+        myTex.writelines(source_lines[indices[i]:indices[i+1]])
+        myTex.close()
+
+
+
+       
+
 
 # Script principal
 def main():
     # On récupère la date au début du traitement
     start_time = datetime.now()
 
-    # On génère le fichier tex à compiler
-    generateTex("test","dnb20Polynesie_ex1")
+    # # On génère le fichier tex à compiler
+    # generateTex("test","dnb20Polynesie_ex1")
 
-    #On compile
-    compilTexToPDF("test")
+    # #On compile
+    # compilTexToPDF("test")
 
-    #On convertit en png
-    pdf2png("test")
+    # #On convertit en png
+    # pdf2png("test")
     
-    #On copie les fichiers pdf, png, pdf-crop dans les bons dossiers
-    copyAllFiles("test")
+    # #On copie les fichiers pdf, png, pdf-crop dans les bons dossiers
+    # copyAllFiles("test")
    
+    # Test 
+    cutTex2Ex("Brevet_Polynesie_sept_2020_DV","Brevet_Polynesie_sept_2020_DV")
 
     # On évalue le temps de traitement
     end_time = datetime.now()
@@ -120,4 +154,5 @@ def main():
     print("=============================================================================")
  
 if __name__ == "__main__":
+    os.system("clear")
     main()

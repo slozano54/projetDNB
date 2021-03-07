85cfd8ea209c2bffbbfd6dd553c5266eab5156a9
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-03-07 06:50:43 +0100

    :alien: modif qualité de sortie png et marges pdfcrop

--- tex_a_compiler/dvi2png.sh
+++ tex_a_compiler/dvi2png.sh
@@ -6,6 +6,10 @@
 #brew install poopler
 ######### Linux #############
 # Je ne sais plus
+# poopler-utils ?
+#http://netpbm.sourceforge.net/doc/pnmtopng.html
+#https://tex.stackexchange.com/questions/11866/compile-a-latex-document-into-a-png-image-thats-as-short-as-possible
 
-pdfcrop $1.pdf || exit
-pdftoppm $1-crop.pdf|pnmtopng -compression 0 > $1.png
+pdfcrop -margin 3 $1.pdf || exit
+#pdftoppm $1-crop.pdf|pnmtopng -compression 0 > $1.png
+pdftoppm -r 300 $1-crop.pdf|pnmtopng -compression 0 > $1.png
